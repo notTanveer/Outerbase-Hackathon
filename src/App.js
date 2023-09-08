b23391a82688ee19a14ae1da5e21757661ed8f78
Yeasir0032 <sdyeasirrahaman@gmail.com>
2023-09-08 10:34:58 +0530

    Student Dashboard Initialization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "./App.scss";
+import StudentDashboard from "./pages/student/dashboard";
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <StudentDashboard />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/student/dashboard.jsx
+++ src/pages/student/dashboard.jsx
@@ -1,1 +1,27 @@
 // Will done by Yeasir
+import React from "react";
+import "./student.scss";
+import { CgProfile } from "react-icons/cg";
+
+function StudentDashboard() {
+  return (
+    <div className="student-dashboard">
+      <div className="student-header">
+        {/* This includes Name roll and Proifle picture */}
+        <div className="name">Student Name</div>
+        <div className="profile-options">
+          <div className="roll">ABC004125</div>
+          <div className="profile-pic">
+            <CgProfile />
+          </div>
+        </div>
+      </div>
+      <div className="course-details">
+        {/* THis includes course details  */}
+        ABC Batch - JEE Advanced
+      </div>
+    </div>
+  );
+}
+
+export default StudentDashboard;
