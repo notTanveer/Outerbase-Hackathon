7903878413c5448b7e7fe26c5477eaf65182f71c
notTanveer <101289209+notTanveer@users.noreply.github.com>
2023-09-08 19:53:18 +0530

    Start Up Page & Profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,1 +1,30 @@
-// done by Yeasir
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchUserData } from '../actions/userActions';
+
+const UserDashboard = ({ user, fetchUserData }) => {
+  useEffect(() => {
+    // Fetch user data when the component mounts
+    fetchUserData();
+  }, [fetchUserData]);
+
+  return (
+    <div>
+      {user ? (
+        <Dashboard />
+      ) : (
+        <p>Loading user data...</p>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = {
+  fetchUserData,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserDashboard);
