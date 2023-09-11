4676dae0a921532c9a485cfcfd44cf1697f9d414
notTanveer <101289209+notTanveer@users.noreply.github.com>
2023-09-11 15:18:04 +0530

    Chat Functionality

---
+++ src/pages/chat/interface.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./chat.css";
+
+function Chat() {
+    return (
+        <body>
+            <div class="chat-app">
+            <aside class="sidebar">
+                <header class="sidebar-header">
+                <h2>Online Users</h2>
+                </header>
+                <ul class="contact-list">
+                </ul>
+            </aside>
+            <section class="chat-area">
+                <header class="chat-header">
+                <h2>100x Chat</h2>
+                </header>
+                <div class="messages">
+                <div class="message received">Hello there!</div>
+                <div class="message sent">Hi, how are you?</div>
+                </div>
+                <div class="message-input">
+                <input
+                    type="text"
+                    id="messageInput"
+                    placeholder="Type a message..."
+                />
+                <button id="sendButton">Send</button>
+                </div>
+            </section>
+            </div>
+            <script src="/socket.io/socket.io.js"></script>
+            <script src="script.js"></script>
+        </body>
+
+    );
+}
+
+export default Chat;
--- src/pages/components/navbar.jsx
+++ src/pages/components/navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.scss";
+
 function Navbar() {
   return (
     <div className="content">
       <div className="navbar">
         <div className="logo">
-          <Link to="/">Scholar Sphere</Link>
+          <Link to="/">ScholarSphere</Link>
         </div>
         <div className="all-links">
           <Link to="/student">Student</Link>
           <Link to="/courses">Courses</Link>
           <Link to="/community">Community</Link>
           <Link to="/library">Library</Link>
+          <Link to="/chat">Chat</Link>
         </div>
         <div className="login">
           <div className="login-button">LOGIN</div>
